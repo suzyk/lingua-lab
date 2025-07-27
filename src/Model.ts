252bbb9f87e831ff18be70bd14b8e3adcac24f5f
suzyk <suzykahng@gmail.com>
2025-07-27 22:35:04 +0300

    refactored to use useReducer. need to fix wrong logic and send dispatch down.

--- src/Model.ts
+++ src/Model.ts
@@ -13,3 +13,28 @@ export enum Card_Types {
   TEXT,
   IMAGE,
 }
+
+// selecteText, selectedImage, cardMatched, matchWrong
+export enum GameActionTypes {
+  SELECTED_TEXT = "selectedText",
+  SELECTED_IMAGE = "selectedImage",
+  MATCHED = "matched",
+  WRONG = "wrong",
+  RESET_SELECTION = "resetSelection",
+}
+
+export type GameAction =
+  | { type: GameActionTypes.SELECTED_TEXT; payload: Word }
+  | { type: GameActionTypes.SELECTED_IMAGE; payload: Word }
+  | { type: GameActionTypes.MATCHED; payload: string }
+  | { type: GameActionTypes.WRONG; payload: boolean }
+  | { type: GameActionTypes.RESET_SELECTION };
+
+export interface WordGameState {
+  words: Word[];
+  selectedText: Word | null; // or number[], depending on your IDs
+  selectedImage: Word | null;
+  matched: string[];
+  wrong: boolean;
+  clickedCards: string[];
+}
--- src/Routes/Games.tsx
+++ src/Routes/Games.tsx
@@ -1,12 +1,68 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import WordCard from "../Components/WordCard";
-import { Card_Types, Word } from "../Model";
+import {
+  Card_Types,
+  Word,
+  GameAction,
+  GameActionTypes,
+  WordGameState,
+} from "../Model";
 import { randomNoRepeats } from "../Util";
 import ConfettiExplosion from "react-confetti-explosion";
 import "../App.css";
 
+const wordReducer = (
+  state: WordGameState,
+  action: GameAction
+): WordGameState => {
+  switch (action.type) {
+    case GameActionTypes.SELECTED_TEXT: {
+      console.log("text selected");
+      return {
+        ...state,
+        selectedText:
+          state.selectedText?.text === action.payload.text
+            ? null
+            : action.payload,
+      };
+    }
+    case GameActionTypes.SELECTED_IMAGE: {
+      console.log("image selected");
+      return {
+        ...state,
+        selectedImage:
+          state.selectedImage?.text === action.payload.text
+            ? null
+            : action.payload,
+      };
+    }
+    case GameActionTypes.MATCHED: {
+      console.log("card mathced!");
+      return { ...state, matched: [...state.matched, action.payload] };
+    }
+    case GameActionTypes.WRONG: {
+      console.log("match wrong!");
+      return {
+        ...state,
+        wrong: false,
+      };
+    }
+    case GameActionTypes.RESET_SELECTION: {
+      console.log("reset selection");
+      return {
+        ...state,
+        selectedImage: null,
+        selectedText: null,
+      };
+    }
+
+    default:
+      return state;
+  }
+};
+
 const Games = () => {
-  const vocab: Word[] = [
+  const words: Word[] = [
     { text: "cat", image_url: "../images/cat.png" },
     { text: "dog", image_url: "../images/dog.png" },
     { text: "fish", image_url: "../images/fish.png" },
@@ -14,42 +70,61 @@ const Games = () => {
     { text: "tree", image_url: "../images/tree.png" },
   ];
   // double the words for text cards and image cards.
-  vocab.push.apply(vocab, vocab);
+  words.push.apply(words, words);
 
-  const [words, setwords] = useState<Word[]>(vocab);
-  const [selectedText, setSelectedText] = useState<Word | null>(null);
-  const [selectedImage, setSelectedImage] = useState<Word | null>(null);
-  const [matched, setMatched] = useState<string[]>([]);
-  const [wrong, setWrong] = useState<boolean>(false);
+  // const [words, setwords] = useState<Word[]>(vocab);
+  const initialState: WordGameState = {
+    words: words,
+    selectedText: null,
+    selectedImage: null,
+    matched: [],
+    wrong: false,
+    clickedCards: [], //status: 'idle' | 'checking' | 'complete'
+  };
+
+  const [state, dispatch] = useReducer(wordReducer, initialState); // set default
+  //const [selectedText, setSelectedText] = useState<Word | null>(null);
+  //const [selectedImage, setSelectedImage] = useState<Word | null>(null);
+  //const [matched, setMatched] = useState<string[]>([]);
+  //const [wrong, setWrong] = useState<boolean>(false);
 
   const [randomizer] = useState(() => randomNoRepeats(words.length));
   const [showOverlay, setShowOverlay] = useState<boolean>(false);
 
-  const allMatched = words.length / 2 === matched.length;
+  const allMatched = words.length / 2 === state.matched.length;
+  /*
   const handleCardClick = (word: Word, type: Card_Types) => {
     if (type === Card_Types.TEXT)
       setSelectedText((prev) => (prev?.text === word.text ? null : word));
     else setSelectedImage((prev) => (prev?.text === word.text ? null : word));
-  };
+  };*/
 
   useEffect(() => {
-    if (selectedText && selectedImage) {
-      if (selectedText.text === selectedImage.text) {
-        console.log("it's a match");
-        setMatched([...matched, selectedText.text]);
+    if (state.selectedText && state.selectedImage) {
+      if (state.selectedText.text === state.selectedImage.text) {
+        //console.log("it's a match");
+        //setMatched([...matched, selectedText.text]);
+        dispatch({
+          type: GameActionTypes.MATCHED,
+          payload: state.selectedText.text,
+        });
       } else {
         // add red color
-        console.log("Wrong!");
-        setWrong(true);
+        //console.log("Wrong!");
+        //setWrong(true);
+        dispatch({ type: GameActionTypes.WRONG, payload: true });
       }
+
       const timer = setTimeout(() => {
+        /*
         setSelectedText(null);
         setSelectedImage(null);
-        setWrong(false);
+        setWrong(false);*/
+        dispatch({ type: GameActionTypes.RESET_SELECTION });
       }, 500);
       return () => clearTimeout(timer);
     }
-  }, [selectedImage, selectedText, wrong]);
+  }, [state.selectedImage, state.selectedText, state.wrong]);
 
   useEffect(() => {
     if (allMatched) {
@@ -60,7 +135,8 @@ const Games = () => {
       return () => clearTimeout(timer);
     }
   }, [allMatched]);
-
+  //onClick={() => handleCardClick(words[randNum], Card_Types.TEXT)}
+  //handleCardClick(words[randNum], Card_Types.IMAGE)
   return (
     <div>
       <h1>Games</h1>
@@ -73,15 +149,23 @@ const Games = () => {
                 key={randNum}
                 word={words[randNum]}
                 type={Card_Types.TEXT}
-                onClick={() => handleCardClick(words[randNum], Card_Types.TEXT)}
-                isSelected={selectedText?.text === words[randNum].text}
-                isMatched={matched.includes(words[randNum].text)}
-                disabled={
-                  matched.includes(words[randNum].text) ||
-                  (selectedText != null &&
-                    selectedText.text !== words[randNum].text)
+                onClick={() =>
+                  dispatch({
+                    type: GameActionTypes.SELECTED_TEXT,
+                    payload: words[randNum],
+                  })
                 }
-                wrong={selectedText?.text === words[randNum].text && wrong}
+                isSelected={state.selectedText?.text === words[randNum].text}
+                isMatched={state.matched.includes(words[randNum].text)}
+                disabled={
+                  state.matched.includes(words[randNum].text) ||
+                  (state.selectedText != null &&
+                    state.selectedText.text !== words[randNum].text)
+                }
+                wrong={
+                  state.selectedText?.text === words[randNum].text &&
+                  state.wrong
+                }
               />
             ))}
         </div>
@@ -94,16 +178,22 @@ const Games = () => {
                 word={words[randNum]}
                 type={Card_Types.IMAGE}
                 onClick={() =>
-                  handleCardClick(words[randNum], Card_Types.IMAGE)
+                  dispatch({
+                    type: GameActionTypes.SELECTED_IMAGE,
+                    payload: words[randNum],
+                  })
                 }
-                isSelected={selectedImage?.text === words[randNum].text}
-                isMatched={matched.includes(words[randNum].text)}
+                isSelected={state.selectedImage?.text === words[randNum].text}
+                isMatched={state.matched.includes(words[randNum].text)}
                 disabled={
-                  matched.includes(words[randNum].text) ||
-                  (selectedImage != null &&
-                    selectedImage.text !== words[randNum].text)
+                  state.matched.includes(words[randNum].text) ||
+                  (state.selectedImage != null &&
+                    state.selectedImage.text !== words[randNum].text)
                 }
-                wrong={selectedImage?.text === words[randNum].text && wrong}
+                wrong={
+                  state.selectedImage?.text === words[randNum].text &&
+                  state.wrong
+                }
               />
             ))}
         </div>
