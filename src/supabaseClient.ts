59f6231980649c6a528bb2afc33b5889b40a1087
suzyk <suzykahng@gmail.com>
2025-09-28 22:07:44 +0300

    catching key error

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -3,4 +3,8 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL; // from dashboard → Settings → API
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY; // from same place
 
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("supabaseUrl or anon key is missing!");
+}
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
